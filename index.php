58598614c2b20c236d714e55ad4aa66dbdf7353a
Smith's Wood Primary Academy <37655367+Smithswood@users.noreply.github.com>
2018-03-23 12:45:05 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
 <h1>My First Heading</h1>
 
 <p>My first paragraph.</p>
-  
-  <img src="[logo_large_nobg.png](images/logo_large_nobg.png)" alt="W3Schools.com" width="250" height="250">
+  html2
+   logo_large_nobg.png.png
+  <img src="![](/logo_large_nobg.png.png)" alt="W3Schools.com" width="250" height="250">
 
 </body>
 </html>
