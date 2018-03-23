e7fdab336dfcb69d17ceb9f4d1423347cfa3a992
Smith's Wood Primary Academy <37655367+Smithswood@users.noreply.github.com>
2018-03-23 14:00:49 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <p>My first paragraph.</p>
 
-  <img src=![logo_large_nobg.png](https://gist.github.com/Smithswood/605560c2961cb3025038/raw/b75d209134f0fb64f04a5ee9ff274ceabf6e26e8/logo_large_nobg.png) alt="W3Schools.com" width="250" height="250">
+  <img src=(logo_large_nobg.png) alt="W3Schools.com" width="250" height="250">
 
 </body>
 </html>
