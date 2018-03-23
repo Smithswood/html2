c7fb49eb9622f69bbeb6a39feb16acd98306132c
Smith's Wood Primary Academy <37655367+Smithswood@users.noreply.github.com>
2018-03-23 12:47:25 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
 <h1>My First Heading</h1>
 
 <p>My first paragraph.</p>
-  html2
-   logo_large_nobg.png
+
   <img src="![/images](/logo_large_nobg.png)" alt="W3Schools.com" width="250" height="250">
 
 </body>
